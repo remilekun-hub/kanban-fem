e5868ed504914e7076cf3496c474ef2372da9830
remilekun-hub <atandaremilekun@gmail.com>
2025-07-18 20:40:50 +0100

    setup dnd toolkit

--- components/ui/mode-toggle.tsx
+++ components/ui/mode-toggle.tsx
@@ -1,22 +1,23 @@
 "use client";
 
-import * as React from "react";
-// import { Moon, Sun } from "lucide-react";
+import React from "react";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Switch } from "@/components/ui/switch";
 import Moon from "@/app/icons/moon";
 import Sun from "@/app/icons/sun";
 
 export function ModeToggle() {
 	const { setTheme, theme } = useTheme();
+	const [isChecked, setIsChecked] = React.useState(theme === "dark");
+	console.log({ theme, isChecked });
+
+	React.useEffect(() => {
+		if (theme === "dark") {
+			setIsChecked(true);
+		} else {
+			setIsChecked(false);
+		}
+	}, [theme, setIsChecked]);
 
 	return (
 		<div className="rounded-[6px] flex justify-center items-center py-3 bg-foreground w-full mr-6 gap-6.5">
@@ -24,7 +25,8 @@ export function ModeToggle() {
 
 			<Sun />
 			<Switch
-				checked={theme === "dark"}
+				key={theme}
+				checked={isChecked}
 				onCheckedChange={() =>
 					setTheme(theme === "dark" ? "light" : "dark")
 				}
